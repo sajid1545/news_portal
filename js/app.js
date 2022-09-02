bd57d634fd7f3e6c70ef44db7327c022d1360f58
sajid9962 <clashking1545@gmail.com>
2022-09-02 23:18:02 +0600

    some cards added

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ loadCategories();
 
 let displayCategories = async () => {
 	let categories = await loadCategories();
-	console.log(categories);
 
 	let categoriesContainer = document.getElementById('categories-container');
 
@@ -27,10 +26,55 @@ let displayCategories = async () => {
 
 		categoryDiv.classList.add('cursor-pointer');
 		categoryLink.classList.add('link', 'link-hover');
-
-		categoryLink.innerHTML = `${category.category_name}`;
+		categoryLink.innerHTML = `
+        <div onclick="loadCategoriesDataById(${category.category_id})">
+        ${category.category_name}
+        </div>
+        
+        
+        `;
 		categoryDiv.appendChild(categoryLink);
 		categoriesContainer.appendChild(categoryDiv);
 	});
 };
 displayCategories();
+
+// loading categories data from https://openapi.programming-hero.com/api/news/${news_id}
+
+let loadCategoriesDataById = (category_id) => {
+	let url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => displayCategoriesNews(data.data))
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+let displayCategoriesNews = (news) => {
+	let displayNewsContainer = document.getElementById('display-news-container');
+	news.forEach((newsItem) => {
+		console.log(newsItem);
+
+		let newsDiv = document.createElement('div');
+		newsDiv.innerHTML = `
+        <div class="card flex flex-row   bg-white shadow-xl my-5 ">
+<figure><img src="${newsItem.thumbnail_url}" alt="Album" ></figure>
+<div class="card-body">
+  <h2 class="card-title text-black font-bold text-2xl">${newsItem.title}</h2>
+  <p>${newsItem.details.length > 20 ? newsItem.details.slice(0, 200) + '...' : newsItem.details}
+    </p>  
+  <p>${newsItem.details.length > 20 ? newsItem.details.slice(200, 400) + '...' : newsItem.details}
+    </p>  
+
+    <div>
+    
+    </div>
+    
+</div>
+</div> 
+        `;
+		displayNewsContainer.appendChild(newsDiv);
+	});
+};
