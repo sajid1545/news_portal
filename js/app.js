b72630ee03bfb0ef0181d5e00012b802ec902dfe
sajid9962 <clashking1545@gmail.com>
2022-09-03 02:31:44 +0600

    made responsive and validation for author name added

--- js/app.js
+++ js/app.js
@@ -23,17 +23,15 @@ let displayCategories = async () => {
 		let categoryLink = document.createElement('a');
 		categoryDiv.classList.add('cursor-pointer');
 		categoryLink.classList.add('link', 'link-hover');
-		categoryDiv.innerHTML = `
+		categoryLink.innerHTML = `
         <div onclick="loadCategoriesDataById('${category.category_id}')">
 
-        <a>
         ${category.category_name}
-        </a>
            
         </div>
         `;
 
-		// categoryDiv.appendChild(categoryLink);
+		categoryDiv.appendChild(categoryLink);
 		categoriesContainer.appendChild(categoryDiv);
 	});
 };
@@ -62,50 +60,70 @@ let displayCategoriesNews = async (news) => {
 	// total number of news available for each categories
 
 	let totalNumberOfNews = document.getElementById('total-categories-item');
-	totalNumberOfNews.classList.add('bg-white', 'text-3xl', 'text-black', 'text-center');
+	totalNumberOfNews.classList.add('bg-white', 'text-2xl', 'text-black', 'text-center');
 	totalNumberOfNews.innerHTML = `
-    <div class=" p-5">
+    <div class=" p-3 my-10">
     ${news.length} items found for category
     </div>
     `;
+	// sorting views
+	news.sort((a, b) => b.total_view - a.total_view);
 
 	news.forEach((newsItem) => {
+		console.log(newsItem);
 		let newsDiv = document.createElement('div');
 		newsDiv.innerHTML = `
-	        <div class="card flex flex-row   bg-white shadow-xl my-5 ">
-	<figure><img src="${newsItem.thumbnail_url}" alt="Album" ></figure>
-	<div class="card-body">
-	  <h2 class="card-title text-black font-bold text-2xl">${newsItem.title}</h2>
-	  <p>${newsItem.details.length > 20 ? newsItem.details.slice(0, 200) + '...' : newsItem.details}
-	    </p>
-	  <p>${newsItem.details.length > 20 ? newsItem.details.slice(200, 400) + '...' : newsItem.details}
-	    </p>
+	        <div class="card flex sm:flex-row p-4 bg-white shadow-xl my-5 ">
+	            <figure><img src="${
+								newsItem.thumbnail_url
+							}" alt="Album" style="width: 100%; height: 100%;"></figure>
+	            <div class="card-body">
+	            <h2 class="card-title text-black font-bold text-2xl">${newsItem.title}</h2>
+	            <p>${
+								newsItem.details.length > 20
+									? newsItem.details.slice(0, 200) + '...'
+									: newsItem.details
+							}
+	                </p>
+	            <p>${
+								newsItem.details.length > 20
+									? newsItem.details.slice(201, 400) + '...'
+									: newsItem.details
+							}
+	                </p>
 
-	    <div class="grid grid-cols-3 items-center text-center mt-3">
+	                <div class="grid grid-cols-1 sm:grid-cols-3 items-center text-center mt-3">
 
-	        <div class="flex gap-5 items-center">
-	            <div>
-	            <img src=" ${newsItem.author.img}" class="w-10 rounded-full">
+	                    <div class="flex justify-center gap-5 items-center">
+	                        <div>
+	                        <img src=" ${newsItem.author.img}" class="w-10 rounded-full">
+	                        </div>
+	                        <div>
+	                        <h1>${
+														newsItem.author.name === null
+															? 'No author'
+															: newsItem.author.name === ''
+															? 'No author'
+															: newsItem.author.name
+													}
+                            </h1>
+	                        </div>
+	                    </div>
+
+	                    <div>
+	                        <h1 class="font-bold text-black text-lg">${
+														newsItem.total_view ? newsItem.total_view + 'M' : 'Total View Not Found'
+													}</h1>
+	                    </div>
+
+	                    <div>
+                        <label for="my-modal-3" class="btn modal-button btn-outline btn-accent">See More</label>
+	                    </div>
+
+	                </div>
+
 	            </div>
-	            <div>
-	            <h1>${newsItem.author ? newsItem.author.name : 'Author not found'}</h1>
-	            </div>
 	        </div>
-
-	        <div>
-	            <h1 class="font-bold text-black text-lg">${
-								newsItem.total_view ? newsItem.total_view : 'Total View Not Found'
-							}M</h1>
-	        </div>
-
-	        <div>
-            <label for="my-modal-3" class="btn modal-button btn-outline btn-accent">See More</label>
-	        </div>
-
-	    </div>
-
-	</div>
-	</div>
 	        `;
 		displayNewsContainer.appendChild(newsDiv);
 	});
