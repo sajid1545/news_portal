39a9bdfcb7011e7582498f30a0a27be40a29f0c7
sajid9962 <clashking1545@gmail.com>
2022-09-03 01:35:55 +0600

    onclick function added

--- js/app.js
+++ js/app.js
@@ -10,38 +10,40 @@ let loadCategories = async () => {
 		console.log(err);
 	}
 };
-loadCategories();
 
 //  Displaying Categories Name in the web page
 
 let displayCategories = async () => {
 	let categories = await loadCategories();
-
 	let categoriesContainer = document.getElementById('categories-container');
+	categoriesContainer.innerHTML = '';
 
 	categories.forEach((category) => {
 		let categoryDiv = document.createElement('div');
 		let categoryLink = document.createElement('a');
-
 		categoryDiv.classList.add('cursor-pointer');
 		categoryLink.classList.add('link', 'link-hover');
-		categoryLink.innerHTML = `
-        <div onclick="(${category.category_id})">
+		categoryDiv.innerHTML = `
+        <div onclick="loadCategoriesDataById('${category.category_id}')">
+
+        <a>
         ${category.category_name}
+        </a>
+           
         </div>
-        
-        
         `;
-		categoryDiv.appendChild(categoryLink);
+
+		// categoryDiv.appendChild(categoryLink);
 		categoriesContainer.appendChild(categoryDiv);
 	});
 };
 displayCategories();
 
 // loading categories data from https://openapi.programming-hero.com/api/news/category/{category_id}
+// loading categories data from https://openapi.programming-hero.com/api/news/category/01
 
-let loadCategoriesDataById = () => {
-	let url = `https://openapi.programming-hero.com/api/news/category/05`;
+let loadCategoriesDataById = (id) => {
+	let url = `https://openapi.programming-hero.com/api/news/category/${id}`;
 
 	fetch(url)
 		.then((res) => res.json())
@@ -51,50 +53,56 @@ let loadCategoriesDataById = () => {
 		});
 };
 
-let displayCategoriesNews = (news) => {
+let displayCategoriesNews = async (news) => {
 	let displayNewsContainer = document.getElementById('display-news-container');
+	displayNewsContainer.innerHTML = '';
+
+	console.log(news);
+
+	// total number of news available for each categories
+
+	let totalNumberOfNews = document.getElementById('total-categories-item');
+	totalNumberOfNews.classList.add('bg-white', 'text-3xl', 'text-black', 'text-center');
+	totalNumberOfNews.innerHTML = `${news.length} items found for category`;
+
 	news.forEach((newsItem) => {
-		console.log(newsItem);
 		let newsDiv = document.createElement('div');
 		newsDiv.innerHTML = `
-        <div class="card flex flex-row   bg-white shadow-xl my-5 ">
-<figure><img src="${newsItem.thumbnail_url}" alt="Album" ></figure>
-<div class="card-body">
-  <h2 class="card-title text-black font-bold text-2xl">${newsItem.title}</h2>
-  <p>${newsItem.details.length > 20 ? newsItem.details.slice(0, 200) + '...' : newsItem.details}
-    </p>  
-  <p>${newsItem.details.length > 20 ? newsItem.details.slice(200, 400) + '...' : newsItem.details}
-    </p>  
+	        <div class="card flex flex-row   bg-white shadow-xl my-5 ">
+	<figure><img src="${newsItem.thumbnail_url}" alt="Album" ></figure>
+	<div class="card-body">
+	  <h2 class="card-title text-black font-bold text-2xl">${newsItem.title}</h2>
+	  <p>${newsItem.details.length > 20 ? newsItem.details.slice(0, 200) + '...' : newsItem.details}
+	    </p>
+	  <p>${newsItem.details.length > 20 ? newsItem.details.slice(200, 400) + '...' : newsItem.details}
+	    </p>
 
-    
-    <div class="grid grid-cols-3 items-center text-center mt-3">
+	    <div class="grid grid-cols-3 items-center text-center mt-3">
 
-        <div class="flex gap-5 items-center">
-            <div>
-            <img src=" ${newsItem.author.img}" class="w-10 rounded-full">
-            </div>
-            <div>
-            <h1>${newsItem.author ? newsItem.author.name : 'Author not found'}</h1>
-            </div>
-        </div>
+	        <div class="flex gap-5 items-center">
+	            <div>
+	            <img src=" ${newsItem.author.img}" class="w-10 rounded-full">
+	            </div>
+	            <div>
+	            <h1>${newsItem.author ? newsItem.author.name : 'Author not found'}</h1>
+	            </div>
+	        </div>
 
-        <div>
-            <h1 class="font-bold text-black text-lg">${
-							newsItem.total_view ? newsItem.total_view : 'Total View Not Found'
-						}M</h1>
-        </div>
+	        <div>
+	            <h1 class="font-bold text-black text-lg">${
+								newsItem.total_view ? newsItem.total_view : 'Total View Not Found'
+							}M</h1>
+	        </div>
 
-        <div>
-        <button class="btn btn-outline btn-accent">See More</button>
-        </div>
+	        <div>
+	        <button class="btn btn-outline btn-accent">See More</button>
+	        </div>
 
-    </div>
-    
-</div>
-</div> 
-        `;
+	    </div>
+
+	</div>
+	</div>
+	        `;
 		displayNewsContainer.appendChild(newsDiv);
 	});
 };
-
-loadCategoriesDataById();
